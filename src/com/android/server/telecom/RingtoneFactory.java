7ef62b2012ba72c09d08bcc037cb8b741ca6b7f5
Ting Yu <ting.x.yu@sony.com>
2018-10-18 16:55:32 +0800

    Telecomm: Phone ringtone setting for Multi SIM device [3/3]
    Support getting incoming call ringtone for each SIM.
    
    Depends-On: I9c6ccff938122332d222853d469ad9a623c7d193
    
    Bug: 118735436
    Test: Manual
    Change-Id: I895996903593eac8e20968605f74bf2fa85000aa

--- src/com/android/server/telecom/RingtoneFactory.java
+++ src/com/android/server/telecom/RingtoneFactory.java
@@ -30,6 +30,7 @@ import android.os.UserManager;
 import android.provider.Settings;
 
 import android.telecom.Log;
+import android.telephony.SubscriptionManager;
 import android.text.TextUtils;
 
 import com.android.internal.annotations.VisibleForTesting;
@@ -91,20 +92,25 @@ public class RingtoneFactory {
         if (ringtone == null) {
             // Contact didn't specify ringtone or custom Ringtone creation failed. Get default
             // ringtone for user or profile.
-            Context contextToUse = hasDefaultRingtoneForUser(userContext) ? userContext : mContext;
+            int subId = mCallsManager.getPhoneAccountRegistrar()
+                    .getSubscriptionIdForPhoneAccount(incomingCall.getTargetPhoneAccount());
+            int phoneId = SubscriptionManager.getPhoneId(subId);
+            Context contextToUse = hasDefaultRingtoneForUserBySlot(userContext, phoneId)
+                    ? userContext : mContext;
             UserManager um = contextToUse.getSystemService(UserManager.class);
             boolean isUserUnlocked = mFeatureFlags.telecomResolveHiddenDependencies()
                     ? um.isUserUnlocked(contextToUse.getUser())
                     : um.isUserUnlocked(contextToUse.getUserId());
             Uri defaultRingtoneUri;
             if (isUserUnlocked) {
-                defaultRingtoneUri = RingtoneManager.getActualDefaultRingtoneUri(contextToUse,
-                        RingtoneManager.TYPE_RINGTONE);
+                defaultRingtoneUri = RingtoneManager.getActualDefaultRingtoneUriBySlot(
+                        contextToUse, RingtoneManager.TYPE_RINGTONE, phoneId);
                 if (defaultRingtoneUri == null) {
                     Log.i(this, "getRingtone: defaultRingtoneUri for user is null.");
                 }
             } else {
-                defaultRingtoneUri = Settings.System.DEFAULT_RINGTONE_URI;
+                defaultRingtoneUri = phoneId == 1 ? Settings.System.DEFAULT_RINGTONE2_URI
+                        : Settings.System.DEFAULT_RINGTONE_URI;
                 if (defaultRingtoneUri == null) {
                     Log.i(this, "getRingtone: Settings.System.DEFAULT_RINGTONE_URI is null.");
                 }
@@ -201,16 +207,18 @@ public class RingtoneFactory {
         return null;
     }
 
-    private boolean hasDefaultRingtoneForUser(Context userContext) {
+    private boolean hasDefaultRingtoneForUserBySlot(Context userContext, int phoneId) {
         if(userContext == null) {
             return false;
         }
+        String ringtoneSetting = phoneId == 1 ? Settings.System.RINGTONE2
+                : Settings.System.RINGTONE;
         if (mFeatureFlags.resolveHiddenDependenciesTwo()) {
             return !TextUtils.isEmpty(Settings.System.getString(userContext.getContentResolver(),
-                    Settings.System.RINGTONE));
+                    ringtoneSetting));
         } else {
             return !TextUtils.isEmpty(Settings.System.getStringForUser(
-                    userContext.getContentResolver(), Settings.System.RINGTONE,
+                    userContext.getContentResolver(), ringtoneSetting,
                     UserUtil.getUserIdFromContext(userContext, mFeatureFlags)));
         }
     }
